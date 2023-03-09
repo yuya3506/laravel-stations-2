9e7576de7c04a894f3aab6e6684e3b4558dda28f
Yoshikawa Taiki <yoshikawataiki@gmail.com>
2023-03-09 22:41:12 +0900

    update

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('practice', function() {
+    return response('practice');
+});
+
+Route::get('practice2', function() {
+    $test = 'practice2';
+    return response($test);
+});
+
+Route::get('practice3', function() {
+    $test = 'test';
+    return response($test);
+});
